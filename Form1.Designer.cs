ea47f831e92611af72d9b209b642fbd80148bc99
Justus Meister <J.Meister.inf22@stud.bbbaden.ch>
2023-09-15 16:18:21 +0200

    ala

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,28 +29,58 @@
         private void InitializeComponent()
         {
             this.StartLabel = new System.Windows.Forms.Label();
+            this.BackgroundBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // StartLabel
             // 
             this.StartLabel.AutoSize = true;
-            this.StartLabel.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.StartLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(11)))), ((int)(((byte)(55)))));
             this.StartLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.StartLabel.Font = new System.Drawing.Font("Fruity microfont", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.StartLabel.Location = new System.Drawing.Point(189, 9);
+            this.StartLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(249)))), ((int)(((byte)(210)))));
+            this.StartLabel.Location = new System.Drawing.Point(185, 35);
             this.StartLabel.Name = "StartLabel";
             this.StartLabel.Size = new System.Drawing.Size(432, 47);
             this.StartLabel.TabIndex = 0;
             this.StartLabel.Text = "Workout Manager";
             // 
+            // BackgroundBox
+            // 
+            this.BackgroundBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(38)))), ((int)(((byte)(74)))));
+            this.BackgroundBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.BackgroundBox.Location = new System.Drawing.Point(81, 25);
+            this.BackgroundBox.Name = "BackgroundBox";
+            this.BackgroundBox.Size = new System.Drawing.Size(641, 69);
+            this.BackgroundBox.TabIndex = 1;
+            this.BackgroundBox.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(27, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(746, 417);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(214)))), ((int)(((byte)(132)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.StartLabel);
+            this.Controls.Add(this.BackgroundBox);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Workout Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,5 +89,7 @@
         #endregion
 
         private Label StartLabel;
+        private PictureBox BackgroundBox;
+        private PictureBox pictureBox1;
     }
 }
